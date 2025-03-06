998f7ff66dbeb00d223c91e1191f1b62ebd43503
hzihhuang <hzihhuang@163.com>
2025-03-06 19:43:23 +0800

    perf: 优化登录注册页面

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -45,50 +45,57 @@ onShow(() => {
 </script>
 
 <template>
-  <view class="flex justify-center mb-48 pt-180">
+  <view class="flex flex-col items-center gap-32 mb-48 pt-180">
     <wd-img
-      width="256rpx"
-      height="256rpx"
+      width="224rpx"
+      height="224rpx"
       radius="100%"
       src="https://nest.nodejs.cn/assets/logo-small-gradient.svg"
     />
+    <view class="fs-48 fw-bold">记账本</view>
   </view>
   <view class="px-64">
     <wd-form ref="formRef" :model="user">
-      <wd-cell-group border>
-        <wd-input
-          label="用户名"
-          label-width="160rpx"
-          prop="username"
-          clearable
-          v-model="user.username"
-          :rules="[
-            { required: true, message: '用户名不能为空' },
-            { required: true, pattern: /^[\s\S]{4,20}$/, message: '用户名必须在 4-20 个字符之间' },
-          ]"
-          placeholder="请输入用户名"
-        />
-        <wd-input
-          label="密码"
-          label-width="160rpx"
-          prop="password"
-          show-password
-          clearable
-          v-model="user.password"
-          :rules="[
-            { required: true, message: '密码不能为空' },
-            {
-              required: true,
-              pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+]{6,32}$/,
-              message: '密码必须包含字母和数字，且长度为 6-32 个字符',
-            },
-          ]"
-          placeholder="请输入密码"
-        />
-      </wd-cell-group>
-      <view class="mt-48 flex flex-col gap-24">
-        <wd-button type="primary" @click="handleLogin" block>登录</wd-button>
-        <wd-button type="info" @click="handleGoRegister" block>注册</wd-button>
+      <wd-input
+        class="radius-18 border px-24 py-8"
+        prefix-icon="user"
+        prop="username"
+        clearable
+        v-model="user.username"
+        :rules="[
+          { required: false, pattern: /^[\s\S]{4,20}$/, message: '用户名必须在 4-20 个字符之间' },
+        ]"
+        placeholder="请输入用户名"
+      />
+      <wd-input
+        class="mt-40 radius-18 border px-24 py-8"
+        prefix-icon="lock-on"
+        prop="password"
+        show-password
+        clearable
+        v-model="user.password"
+        :rules="[
+          {
+            required: false,
+            pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+]{6,32}$/,
+            message: '密码必须包含字母和数字，且长度为 6-32 个字符',
+          },
+        ]"
+        placeholder="请输入密码"
+      />
+      <wd-button
+        class="mt-50"
+        size="large"
+        type="primary"
+        @click="handleLogin"
+        :round="false"
+        block
+      >
+        登录
+      </wd-button>
+      <view class="mt-40 fs-28 text-center">
+        还没有账号？
+        <text class="color-primary" @click="handleGoRegister">立即注册</text>
       </view>
     </wd-form>
   </view>
--- src/pages/register.vue
+++ src/pages/register.vue
@@ -43,67 +43,73 @@ const handleBack = () =>
 </script>
 
 <template>
-  <view class="flex justify-center mb-48 mt-180">
+  <view class="flex flex-col items-center gap-32 mb-48 pt-180">
     <wd-img
-      width="256rpx"
-      height="256rpx"
+      width="224rpx"
+      height="224rpx"
       radius="100%"
       src="https://nest.nodejs.cn/assets/logo-small-gradient.svg"
     />
+    <view class="fs-48 fw-bold">记账本</view>
   </view>
   <view class="px-64">
     <wd-form ref="formRef" :model="registerUser">
-      <wd-cell-group border>
-        <wd-input
-          label="用户名"
-          label-width="160rpx"
-          prop="username"
-          clearable
-          v-model="registerUser.username"
-          :rules="[
-            { required: true, message: '用户名不能为空' },
-            { required: true, pattern: /^[\s\S]{4,20}$/, message: '用户名必须在 4-20 个字符之间' },
-          ]"
-          placeholder="请输入用户名"
-        />
-        <wd-input
-          label="密码"
-          label-width="160rpx"
-          prop="password"
-          show-password
-          clearable
-          v-model="registerUser.password"
-          :rules="[
-            { required: true, message: '密码不能为空' },
-            {
-              required: true,
-              pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+]{6,32}$/,
-              message: '密码必须包含字母和数字，且长度为 6-32 个字符',
-            },
-          ]"
-          placeholder="请输入密码"
-        />
-        <wd-input
-          label="确认密码"
-          label-width="160rpx"
-          prop="confirmPassword"
-          show-password
-          clearable
-          v-model="registerUser.confirmPassword"
-          :rules="[
-            { required: true, message: '确认密码不能为空' },
-            {
-              required: true,
-              validator: (value) => value === registerUser.password,
-              message: '两次输入的密码不一致',
-            },
-          ]"
-          placeholder="请输入确认密码"
-        />
-      </wd-cell-group>
-      <view class="mt-48 flex flex-col gap-24">
-        <wd-button type="primary" @click="handleRegister" block>确认注册</wd-button>
-        <wd-button type="info" @click="handleBack" block>返回</wd-button>
+      <wd-input
+        class="radius-18 border px-24 py-8"
+        prefix-icon="user"
+        prop="username"
+        clearable
+        v-model="registerUser.username"
+        :rules="[
+          { required: false, pattern: /^[\s\S]{4,20}$/, message: '用户名必须在 4-20 个字符之间' },
+        ]"
+        placeholder="请输入用户名"
+      />
+      <wd-input
+        class="radius-18 border px-24 py-8 mt-40"
+        prefix-icon="lock-on"
+        prop="password"
+        show-password
+        clearable
+        v-model="registerUser.password"
+        :rules="[
+          {
+            required: false,
+            pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+]{6,32}$/,
+            message: '密码必须包含字母和数字，且长度为 6-32 个字符',
+          },
+        ]"
+        placeholder="请输入密码"
+      />
+      <wd-input
+        class="radius-18 border px-24 py-8 mt-40"
+        prefix-icon="lock-on"
+        prop="confirmPassword"
+        show-password
+        clearable
+        v-model="registerUser.confirmPassword"
+        :rules="[
+          {
+            required: false,
+            validator: (value) => value === registerUser.password,
+            message: '两次输入的密码不一致',
+          },
+        ]"
+        placeholder="请输入确认密码"
+      />
+      <wd-button
+        class="mt-50"
+        size="large"
+        :round="false"
+        type="primary"
+        @click="handleRegister"
+        block
+      >
+        注册
+      </wd-button>
+      <view class="mt-40 fs-28 text-center">
+        还没有账号？
+        <text class="color-primary" @click="handleBack">返回登录</text>
       </view>
     </wd-form>
   </view>
